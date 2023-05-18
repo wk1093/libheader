9b29be36eb9b14b12a56867bcd0164feaf03c593
wk1093 <wyattk1093@gmail.com>
2023-05-18 16:02:35 -0700

    asdf

--- libheader/__main__.py
+++ libheader/__main__.py
@@ -3,7 +3,7 @@ import sys
 if len(sys.argv) > 1:
     if sys.argv[1] == "gcc":
         sys.argv = sys.argv[1:]
-        import gcclh
+        from libheader import gcclh
     elif sys.argv[1] == "lib" or sys.argv[1] == "make":
         sys.argv = sys.argv[1:]
-        import makelh
+        from libheader import makelh
