d4424cc4b11ff6fd93e54123cd44305a276f31f1
wk1093 <wyattk1093@gmail.com>
2023-05-20 17:00:54 -0700

    multiple files

--- libheader/makelh.py
+++ libheader/makelh.py
@@ -3,19 +3,21 @@ from . import lh
 import os
 import sys
 
-if len(sys.argv) != 3:
+if len(sys.argv) < 3:
     print("USAGE: ")
-    print("makelh [file.h] [file.c]")
+    print("makelh [file.h] [*.c]")
     exit()
 
 with open(sys.argv[1], "r") as f:
     header = f.read()
 
-name = '.'.join(sys.argv[2].split('.')[:-1])
+name = '.'.join(sys.argv[1].split('.')[:-1])
 
-os.system(f"gcc -c {sys.argv[2]} -o temp.o")
-os.system(f"ar rcs {name}.lib temp.o")
-os.remove("temp.o")
+for i in sys.argv[2:]:
+    os.system(f"gcc -c {i} -o {i}.o")
+os.system(f"ar rcs {name}.lib {' '.join([i+'.o' for i in sys.argv[2:]])}")
+for i in sys.argv[2:]:
+    os.remove(f"{i}.o")
 
 with open(f"{name}.lib", "rb") as f:
     lib = f.read()
